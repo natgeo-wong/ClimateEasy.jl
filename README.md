98d8d1ad1c38ff497a0ad63d96dfd1c41de46020
natgeo-wong <natgeo.wong@outlook.com>
2020-03-02 20:23:08 -0500

    Deprecating ClimateEasy.jl in favour of GeoRegions.jl

--- README.md
+++ README.md
@@ -1,23 +1,3 @@
 # ClimateEasy
 
-NOTE: BREAKING CHANGES FOR V0.2
-
-ClimateEasy contains scripts and functions that aim to automate the basic parts
-of coding in climate research.  It currently has the following functionalities:
-* Conversion of dates to formatted strings
-* Extraction of regional data from a given dataset
-* Extraction of data from HDF4 legacy format
-* Mapping of gridded data using Julia's inbuilt Python functionalities
-
-I will be adding Documentation to this later.
-
-Currently, the ClimateEasy.jl package requires the following Julia dependencies:
-* Dates, DelimitedFiles, Printf
-* PyCall, PyPlot
-
-The ClimateEasy.jl package uses the following Python dependencies:
-* Cartopy
-* pyhdf
-
-Author(s):
-* Nathanael Zhixin Wong: nathanaelwong@fas.harvard.edu
+**NB: ClimateEasy.jl has been deprecated in favour of GeoRegions.jl, which contains the most important functionality - selection and extraction of regional information.**
--- src/ClimateEasy.jl
+++ src/ClimateEasy.jl
@@ -1,49 +1,5 @@
 module ClimateEasy
 
-# Main file for the ClimateEasy module that provides a range of useful
-# commands written in Julia that allows one to easily perform commonly-needed
-# tasks such as:
-#   - Converting dates and times to string format (e.g. YYYYMMDD)
-#   - Generating vectors of dates
-#   - Decomposing datestrings into their relevant components
-#   - Plotting on maps (requires CartoPy in conda package manager)
-
-## Modules Used
-using Dates, DelimitedFiles, Printf, Statistics
-using PrettyTables
-using PyCall, PyPlot
-
-## Adding relevant Python Functions
-const ccrs  = PyNULL()
-const pyhdf = PyNULL()
-
-function __init__()
-    copy!(ccrs,pyimport_conda("cartopy","cartopy"))
-    copy!(pyhdf,pyimport_conda("pyhdf","pyhdf","conda-forge"))
-end
-
-## Exporting functions
-export
-       yr2str, mo2str, dy2str, hr2str, mi2str, yrmo2str, ymd2str, ymdhm2str,
-       yrmo2dir, ymd2dir, yrdy2dir,
-       dntsea2mon,
-       regioninfoload, regioninfodisplay, regioninfoall, regioninfoadd,
-       regionshortname, regionfullname, regionparent,
-       regionbounds, regionpoint, regiongrid, regionisglobe,
-       ispointinregion, isgridinregion,
-       regiongridvec, regionextract, regionextractpoint, regionextractgrid,
-       from0360to180, from180to0360,
-       convert2hourly, convert23hourly, convert2daily,
-       convert2regionmean, convert2zonalmean, convert2meridionalmean,
-       mollweide, robinson, coastlines,
-       h4read, nanop, real2int16!
-
-include("datetime.jl")
-include("regions.jl")
-include("processing.jl")
-include("hdf4.jl")
-include("mapping.jl")
-include("misc.jl")
-include("real2int.jl")
+error("ClimateEasy.jl has been deprecated in favour of GeoRegions.jl.")
 
 end # module
