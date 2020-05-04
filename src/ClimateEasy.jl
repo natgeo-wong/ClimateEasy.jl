029cf1a22d0e635d872f331343c054adaf819841
Nathanael Wong <natgeo.wong@outlook.com>
2020-05-04 00:55:46 -0400

    Anyone who installs and uses ClimateEasy will now automatically install GeoRegions

--- src/ClimateEasy.jl
+++ src/ClimateEasy.jl
@@ -1,6 +1,8 @@
 module ClimateEasy
 
-using Logging
-@warn "ClimateEasy.jl has been deprecated in favour of GeoRegions.jl."
+using Pkg, Logging
+@warn "ClimateEasy.jl has been deprecated in favour of GeoRegions.jl, installing GeoRegions.jl now ..."
+
+Pkg.add("GeoRegions")
 
 end # module
